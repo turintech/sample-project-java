bf914e5d629288eaf0b6dac7945f3429ab93e409
mike <39063587+mike-turintech@users.noreply.github.com>
2025-08-21 12:24:27 +0100

    feat: add prime checking and summation methods

--- app/src/main/java/algorithms/Primes.java
+++ app/src/main/java/algorithms/Primes.java
@@ -1,4 +1,5 @@
 package algorithms;
+import java.util.Arrays;
 import java.util.Vector;
 
 public class Primes {
@@ -12,7 +13,13 @@ public class Primes {
     if (n < 2) {
       return false;
     }
-    for (int i = 2; i * i <= n; i++) { // Optimized loop condition
+    if (n == 2) {
+      return true;
+    }
+    if (n % 2 == 0) {
+      return false;
+    }
+    for (int i = 3; i * i <= n; i += 2) { // Optimized loop condition
       if (n % i == 0) {
         return false;
       }
@@ -27,10 +34,24 @@ public class Primes {
    * @return The sum of the first n prime numbers.
    */
   public static int SumPrimes(int n) {
+    if (n < 3) {
+      return 0;
+    }
+    boolean[] isPrime = new boolean[n];
+    Arrays.fill(isPrime, true);
+
+    for (int p = 2; p * p < n; p++) {
+      if (isPrime[p]) {
+        for (int i = p * p; i < n; i += p) {
+          isPrime[i] = false;
+        }
+      }
+    }
+
     int sum = 0;
-    for (int i = 0; i < n; i++) {
-      if (IsPrime(i)) {
-        sum = sum + i;
+    for (int i = 2; i < n; i++) {
+      if (isPrime[i]) {
+        sum += i;
       }
     }
     return sum;
@@ -43,10 +64,10 @@ public class Primes {
    * @return An vector of all prime factors of n.
    */
   public static Vector<Integer> PrimeFactors(int n) {
-    Vector<Integer> ret = new Vector<Integer>();
+    Vector<Integer> ret = new Vector<>();
 
     for (int i = 2; i * i <= n; i++) { // Optimized loop condition
-      while (n % i == 0 && IsPrime(i)) { // Optimized to handle repeated factors
+      while (n % i == 0) { // Optimized to handle repeated factors
         ret.add(i);
         n /= i; // Reduce n to avoid redundant checks.
       }
